b0f9533406a2e9f5e73d23c5015409d03ac18d41
Nick <believewithyoureyes@gmail.com>
2018-02-01 19:23:49 -0500

    started adding admin routes

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 
+/*
+ * Controller references
+ */
+const Homepage = require('../app/http/controllers/admin/Homepage');
+const Ministries = require('../app/http/controllers/admin/Ministries'); 
+const About = require('../app/http/controllers/admin/About');
+const Photos = require('../app/http/controllers/admin/Photos');
+const Worship = require('../app/http/controllers/admin/Worship');
+const Rentals = require('../app/http/controllers/admin/Rentals');
+const Staff = require('../app/http/controllers/admin/Staff');
+const Newsletter = require('../app/http/controllers/admin/Newsletter'); 
+const LoginPage = require('../app/http/controllers/admin/Login'); 
+const AdminAccount = require('../app/http/controllers/admin/AdminAccount'); 
+const MainDash = require('../app/http/controllers/admin/MainDash'); 
+ 
+/*
+ * middleware
+ */
+const Auth = require('../app/http/middleware/Auth'); // redirect to login page
+
+router.use('/admin/', Auth.isAuthenticated);
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/admin/', MainDash.index);
 
 module.exports = router;
 
