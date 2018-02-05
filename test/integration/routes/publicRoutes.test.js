9f493318aaaefeff9d9971ae43319619ff98abbd
Nick <believewithyoureyes@gmail.com>
2018-02-04 23:09:29 -0500

    created test for error handler

--- test/integration/routes/publicRoutes.test.js
+++ test/integration/routes/publicRoutes.test.js
@@ -3,10 +3,18 @@ const app       = require(`${root}/app`);
 const request   = require('supertest')(app);
 
 
-describe('Public Homepage route', () => {
+describe('#Public Homepage route', () => {
     it('returns the homepage', done => {
         request.get('/')
             .expect(200)
             .expect(/Heading/, done);
+    });
+});
+
+
+describe("#404 Not Found", () => {
+    it('fake route should respond with 404', done => {
+        request.get('/fake-route')
+            .expect(404, done);
     });
 });
