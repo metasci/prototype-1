eb766cd694603ba1f8c23e1187368a814f471456
Nick <believewithyoureyes@gmail.com>
2018-02-04 23:30:49 -0500

    fixed file extensions, and added route tests

--- test/integration/routes/publicRoutes.test.js
+++ test/integration/routes/publicRoutes.test.js
@@ -3,11 +3,50 @@ const app       = require(`${root}/app`);
 const request   = require('supertest')(app);
 
 
-describe('#Public Homepage route', () => {
+describe('#Public Routes', () => {
     it('returns the homepage', done => {
         request.get('/')
             .expect(200)
             .expect(/Heading/, done);
+    });
+
+    it('returns the calendar page', done => {
+        request.get('/calendar')
+            .expect(200, done);
+    });
+
+    it('returns the ministries page', done => {
+        request.get('/ministries')
+            .expect(200, done);
+    });
+
+    it('returns the about page', done => {
+        request.get('/about')
+            .expect(200, done);
+    });
+
+    it('returns the photos page', done => {
+        request.get('/photos')
+            .expect(200, done);
+    });
+
+    it('returns the worship page', done => {
+        request.get('/worship')
+            .expect(200, done);
+    });
+    
+    it('returns the rentals page', done => {
+        request.get('/rentals')
+            .expect(200, done);
+    });
+    it('returns the contact page', done => {
+        request.get('/contact')
+            .expect(200, done);
+    });
+    
+    it('returns the staff page', done => {
+        request.get('/staff')
+            .expect(200, done);
     });
 });
 
