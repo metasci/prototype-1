e2d45ef25b7c84c97923adf6a06ac11a87e12e05
Nick <believewithyoureyes@gmail.com>
2018-03-10 22:13:22 -0500

    added ministries, rentals, worship

--- app/http/controllers/public/Worship.js
+++ app/http/controllers/public/Worship.js
@@ -2,11 +2,17 @@
 module.exports = {
 	
 	index: (req, res, next) => {
-	  res.render('pages/public/worship');
-	},
+	
+		let info = {
+			communion: ["11 AM saturday","12 AM sunday"],
+			service: ["1 PM wednesday", "4 PM friday"]
+		}
+		
+		let docs = {
+			bulletin: "https://www.google.com",
+			refrigerator: "https://www.stackoverflow.com"
+		}
+		res.render('pages/public/worship', {times:info, docs:docs});
+	}
 
-	/*
-	 * here only for unit example test
-	 */
-	alwaysTrue: () => true 
 }
--- app/webpack/public-entry.js
+++ app/webpack/public-entry.js
@@ -2,23 +2,8 @@
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'popper.js';
 
 
-/*
- *import $ from 'jquery';
- *global.$ = global.jQuery = $;
- */
 
-
-// don't import anything new below this line
 import './asset_list.js'; // list of autoloaded assets
-//import '../../resources/assets/public/scripts/jquery.min.js';
-/*
- *import '../../resources/assets/public/scripts/jquery.dropotron.min.js';
- *import '../../resources/assets/public/scripts/jquery.scrollex.min.js';
- *import '../../resources/assets/public/scripts/jquery.scrolly.min.js';
- *import '../../resources/assets/public/scripts/jquery.selectorr.min.js';
- *import '../../resources/assets/public/scripts/skel.min.js';
- *import '../../resources/assets/public/scripts/util.js';
- *import '../../resources/assets/public/scripts/main.js';
- */
