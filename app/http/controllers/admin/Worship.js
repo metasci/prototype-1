e5c132998ee17058ad51bc06ac281a8df3efbae7
Nick <believewithyoureyes@gmail.com>
2018-03-22 17:52:19 -0400

    added worship page deets

--- app/http/controllers/admin/Worship.js
+++ app/http/controllers/admin/Worship.js
@@ -2,11 +2,17 @@
 module.exports = {
 	
 	index: (req, res, next) => {
-	  res.render('pages/admin/worship');
-	},
 
-	/*
-	 * here only for unit example test
-	 */
-	alwaysTrue: () => true 
+		res.locals.times = {
+			communion: ["11 AM saturday","12 AM sunday"],
+			service: ["1 PM wednesday", "4 PM friday"]
+		}
+		
+		res.locals.docs = {
+			bulletin: "https://www.google.com",
+			refrigerator: "https://www.stackoverflow.com"
+		}
+
+	  res.render('pages/admin/worship');
+	}
 }
--- app/http/controllers/public/Worship.js
+++ app/http/controllers/public/Worship.js
@@ -3,16 +3,17 @@ module.exports = {
 	
 	index: (req, res, next) => {
 	
-		let info = {
+		res.locals.times = {
 			communion: ["11 AM saturday","12 AM sunday"],
 			service: ["1 PM wednesday", "4 PM friday"]
 		}
 		
-		let docs = {
+		res.locals.docs = {
 			bulletin: "https://www.google.com",
 			refrigerator: "https://www.stackoverflow.com"
 		}
-		res.render('pages/public/worship', {times:info, docs:docs});
+		
+		res.render('pages/public/worship');
 	}
 
 }
