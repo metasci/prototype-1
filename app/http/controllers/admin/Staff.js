d96fc0b33aec25ed984def412db0c8d643d2bc07
Nick <believewithyoureyes@gmail.com>
2018-04-06 17:04:25 -0400

    added image upload functionality to staff page - admin

--- app/http/controllers/admin/Staff.js
+++ app/http/controllers/admin/Staff.js
@@ -1,5 +1,8 @@
-const root 		= require('app-root-path');
-const models 	= require(root + '/database/models');
+const root 			= require('app-root-path');
+const models 		= require(root + '/database/models');
+const fs 			= require('fs');
+const formidable 	= require('formidable');
+const uniqid 		= require('uniqid');
 
 module.exports = {
 
@@ -26,18 +29,36 @@ module.exports = {
 
 	
 	create: (req, res, next) => {
-		
+		// save new staff details
 		let newStaff = {
-			image: req.body.image,
 			name: req.body.name,
 			title: req.body.title,
 			description: req.body.description
 		}
 		
-		// add new Staff to db here
-		console.log(newStaff);
-		
-		res.redirect('/admin/staff');
+		// retrieve file being uploaded
+		let file = new formidable.IncomingForm();
+		file.parse(req, (err, fields, files) => {
+			if(err) throw err;
+
+			// save image name to db
+			newStaff.photo = `${uniqid()}_${files.image.name}`;
+			// add new Staff to db here
+			models.Staff.create(newStaff).then(()=>{
+
+				// save image to root/public/storage/images/staff
+				let oldpath = files.image.path;
+				let newpath = `${root}/public/storage/images/staff/${newStaff.photo}`;
+				fs.copyFile(oldpath, newpath, err => {
+					if(err) throw err;
+					fs.unlink(oldpath, err => {
+						if(err) throw err;
+						res.redirect('/admin/staff');
+					});
+				});
+
+			});
+		});	
 	},
 
 	destroy: (req, res, next) => {
