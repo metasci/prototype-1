3b47bafd10f5453d0e204ce2b99d15affad19c37
Nick <believewithyoureyes@gmail.com>
2018-04-06 18:37:04 -0400

    finished add/delete functionality on music page - admin

--- app/http/controllers/admin/Music.js
+++ app/http/controllers/admin/Music.js
@@ -1,39 +1,50 @@
+const root 		= require('app-root-path');
+const models 	= require(root + '/database/models');
 
 
 
 module.exports = {
 	
+	/**
+     * Retrieve music info from database to display on admin page
+     */
 	index: (req, res, next) => {
-        
-        res.locals.musicItem = {
-            "Choir Rehearsal": decodeURI("%3Cp%3EAnim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.%3C/p%3E"),
-            "Organ": decodeURI("%3Cp%3EAnim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.%3C/p%3E")
-        }
 
-        res.render('pages/admin/music');
+        res.locals.musicItem = {};
+
+        models.Music.findAll().then(results => {
+            results.forEach(item => {
+                res.locals.musicItem[item.get().title] = decodeURI(item.get().description);
+            });
+        }).finally(()=>{
+            res.render('pages/admin/music');
+        });
     },
+
 
     /**
 	 * respond to post
 	 * add new music to database
 	 */
 	create: (req, res, next) => {
-		let newMusicItem = {
+		let newMusic = {
 			title: req.body.title,
 			description: req.body.description
 		}
-		// add new music item to database here
-        console.log(newMusicItem);
-        
-		res.sendStatus(200);
+
+		// add new ministry to database here
+		models.Music.create(newMusic).then(()=>{
+			res.sendStatus(200);
+		});
 	},
 	
 
+	/**
+	 * DELETE MUSIC
+	 */
 	destroy: (req, res, next) => {
-		console.log(req.body.title);
-
-		// delete ministry by title
-
-		res.sendStatus(200);
+		models.Music.destroy({where:{title:req.body.title}}).then(()=>{
+			res.sendStatus(200);
+		});
 	}
 }
--- app/http/controllers/public/Music.js
+++ app/http/controllers/public/Music.js
@@ -2,7 +2,10 @@ const root 		= require('app-root-path');
 const models 	= require(root + '/database/models');
 
 module.exports = {
-	
+    
+    /**
+     * Retrieve music info from database to display on admin page
+     */
 	index: (req, res, next) => {
 
         res.locals.musicItem = {};
