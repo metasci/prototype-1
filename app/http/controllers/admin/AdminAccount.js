ea86ec51d573c577b1970f2aec2b85288777dd6b
Nick <believewithyoureyes@gmail.com>
2018-04-08 23:40:38 -0400

    fixed change password functionality && navbar active page highlight - admin

--- app/http/controllers/admin/AdminAccount.js
+++ app/http/controllers/admin/AdminAccount.js
@@ -5,25 +5,35 @@ const bcrypt = require('bcrypt');
 module.exports = {
 	
 	index: (req, res, next) => {
+		if(req.session && req.session.error)
+			res.locals.error = req.session.error;
+
 	  res.render('pages/admin/account');
 	},
 
 	update: (req, res, next) => {
 
 		
-		// make compare hash the passwd from db
-		// bcrypt.compare(req.body.current, hash, (err, res) => {
-		// 	if(res) {
-				bcrypt.hash(req.body.passwd, 10, function(err, hash) {
-					models.Admin.findOne().then( admin =>{ // maybe move this out of the compare
+		models.Admin.findOne().then( admin =>{
+
+			// make compare current the passwd from db
+			bcrypt.compare(req.body.current, admin.get().passwd, (err, result) => {
+				if(result) {
+					delete req.session.error;
+					// current passwd is correct
+					// hash new passwd
+					bcrypt.hash(req.body.passwd, 10, function(err, hash) {
+						// save new passwd to db
 						admin.update({passwd: hash}).then(()=>{
 							res.redirect('/admin/account');
 						});
 					});
-				});
-			// } else {
-			// 	// add something here to handle incorrect current passwd
-			// }
-		// });
+				} else {
+					// current passwd is incorrect
+					req.session.error = "The current password you entered was incorrect.";
+					res.redirect('/admin/account');
+				}
+			});
+		});
 	}
 }
