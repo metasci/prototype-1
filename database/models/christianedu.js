0c9e2b0557b60a15ce9162f44bb2b6c9100a7bc0
Nick <believewithyoureyes@gmail.com>
2018-07-14 21:20:28 -0400

    cleaned up data retrieval

--- database/models/christianedu.js
+++ database/models/christianedu.js
@@ -1,14 +1,23 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var ChristianEdu = sequelize.define('ChristianEdu', {
-    title: DataTypes.TEXT,
-    description: DataTypes.TEXT
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
-  return ChristianEdu;
+    var ChristianEdu = sequelize.define('ChristianEdu', {
+        title: DataTypes.TEXT,
+        description: DataTypes.TEXT
+    }, {
+        classMethods: {
+            associate: function(models) {
+                // associations can be defined here
+            },
+            getChristianEdu: function () {
+                return this.findAll({attributes: ['title', 'description']}).then(results => {
+                    let christEdu = {};
+                    results.forEach(item => {
+                        christEdu[item.get().title] = decodeURI(item.get().description);
+                    });
+                    return christEdu;
+                });
+            }
+        }
+    });
+    return ChristianEdu;
 };
